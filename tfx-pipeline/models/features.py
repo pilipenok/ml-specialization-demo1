e82b634458c6b9696896c7e6587feb0b26664207
Aleh Pilipionak <aleh_pilipionak@epam.com>
2021-07-27 09:57:11 +0000

    Ml dev

--- tfx-pipeline/models/features.py
+++ tfx-pipeline/models/features.py
@@ -22,51 +22,37 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from typing import Text, List
+import tensorflow as tf
+from tensorflow_transform.tf_metadata import schema_utils
+import tensorflow_transform as tft
 
-# At least one feature is needed.
-
-# Name of features which have continuous float values. These features will be
-# used as their own values.
-DENSE_FLOAT_FEATURE_KEYS = ['avg_fare_per_trip']
-
-# Name of features which have continuous float values. These features will be
-# bucketized using `tft.bucketize`, and will be used as categorical features.
-BUCKET_FEATURE_KEYS = []
-# Number of buckets used by tf.transform for encoding each feature. The length
-# of this list should be the same with BUCKET_FEATURE_KEYS.
-BUCKET_FEATURE_BUCKET_COUNT = []
-
-# Name of features which have categorical values which are mapped to integers.
-# These features will be used as categorical features.
-CATEGORICAL_FEATURE_KEYS = ['pickup_community_area', 'day_of_week', 'month', 'hour_of_day']
-# Number of buckets to use integer numbers as categorical features. The length
-# of this list should be the same with CATEGORICAL_FEATURE_KEYS.
-CATEGORICAL_FEATURE_MAX_VALUES = [100, 7, 2, 12, 24, 2]
-
-# Name of features which have string values and are mapped to integers.
-VOCAB_FEATURE_KEYS = ['am_pm','is_us_holiday']
-
-# Number of vocabulary terms used for encoding VOCAB_FEATURES by tf.transform
-VOCAB_SIZE = 2
-
-# Count of out-of-vocab buckets in which unrecognized VOCAB_FEATURES are hashed.
-OOV_SIZE = 10
-
-# Keys
-LABEL_KEY = 'number_of_trips'
+from pipeline import components
 
 
-def transformed_name(key: Text) -> Text:
-  """Generate the name of the transformed feature from original name."""
-  return key + '_xf'
+FEATURE_KEYS = 'area,is_holiday,day_of_week,year,month,day,hour24,hour12,day_period,avg_total_per_trip_prev4h_area,avg_total_per_trip_prev4h_city,avg_ntrips_prev_4h_area,avg_ntrips_prev_4h_city'.split(',')
 
 
-def vocabulary_name(key: Text) -> Text:
-  """Generate the name of the vocabulary feature from original name."""
-  return key + '_vocab'
+LABEL_KEY = 'relative_demand'
 
 
-def transformed_names(keys: List[Text]) -> List[Text]:
-  """Transform multiple feature names at once."""
-  return [transformed_name(key) for key in keys]
+# Since we're not generating or creating a schema, we will instead create a feature spec.
+FEATURE_SPEC = {
+    **{
+        feature: tf.io.FixedLenFeature(shape=[1], dtype=tf.float32)
+        for feature in 'avg_total_per_trip_prev4h_area,avg_total_per_trip_prev4h_city,avg_ntrips_prev_4h_area,avg_ntrips_prev_4h_city'.split(',')
+    },
+    **{
+        feature: tf.io.FixedLenFeature(shape=[1], dtype=tf.int64)
+        for feature in 'area,day_of_week,year,month,day,hour24,hour12'.split(',')
+    },
+    'is_holiday': tf.io.FixedLenFeature(shape=[1], dtype=tf.string),
+    'day_period': tf.io.FixedLenFeature(shape=[1], dtype=tf.string),
+    LABEL_KEY: tf.io.FixedLenFeature(shape=[1], dtype=tf.float32)
+}
+
+
+def get_schema():
+    return schema_utils.schema_from_feature_spec(FEATURE_SPEC)
+    #return components.schema_gen().outputs['schema']
+
+
---
+++ tfx-pipeline/pipeline/components.py
@@ -0,0 +1,147 @@
+from tfx.components import CsvExampleGen, StatisticsGen, SchemaGen, ExampleValidator, Transform, Trainer, Evaluator, Pusher
+from tfx.proto import trainer_pb2
+from tfx.dsl.components.base import executor_spec
+from tfx.extensions.google_cloud_ai_platform.trainer import executor as ai_platform_trainer_executor
+from tfx.dsl.components.common import resolver
+from tfx.dsl.experimental import latest_blessed_model_resolver
+from tfx.types import Channel
+from tfx.types.standard_artifacts import Model, ModelBlessing
+import tensorflow_model_analysis as tfma
+from tfx.extensions.google_cloud_ai_platform.pusher import executor as ai_platform_pusher_executor
+
+from pipeline import configs
+#from models.features import LABEL_KEY
+LABEL_KEY = 'relative_demand'
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def example_gen():
+    # Brings data into the pipeline or otherwise joins/converts training data.
+    return CsvExampleGen(
+        input_base=configs.DATA_PATH
+    )
+
+
+@lru_cache(maxsize=None)
+def statistics_gen(
+        example_gen=example_gen()
+):
+    # Computes statistics over data for visualization and example validation.
+    return StatisticsGen(
+        examples=example_gen.outputs['examples']
+    )
+
+
+@lru_cache(maxsize=None)
+def schema_gen(
+        statistics_gen=statistics_gen()
+):
+    # Generates schema based on statistics files.
+    return SchemaGen(
+        statistics=statistics_gen.outputs['statistics'],
+        infer_feature_shape=True
+    )
+
+
+@lru_cache(maxsize=None)
+def example_validator(
+        statistics_gen=statistics_gen(),
+        schema_gen=schema_gen()
+):
+    # Performs anomaly detection based on statistics and data schema.
+    return ExampleValidator(
+        statistics=statistics_gen.outputs['statistics'],
+        schema=schema_gen.outputs['schema']
+    )
+
+
+@lru_cache(maxsize=None)
+def transform(
+        example_gen=example_gen(),
+        schema_gen=schema_gen()
+):
+    # Performs transformations and feature engineering in training and serving.
+    return Transform(
+        examples=example_gen.outputs['examples'],
+        schema=schema_gen.outputs['schema'],
+        preprocessing_fn=configs.PREPROCESSING_FN
+    )
+
+
+@lru_cache(maxsize=None)
+def trainer(
+        example_gen=example_gen(),
+        schema_gen=schema_gen(),
+        # transform=transform()
+):
+    return Trainer(
+        run_fn=configs.RUN_FN,
+        examples=example_gen.outputs['examples'],
+        # transformed_examples=transform.outputs['transformed_examples'],
+        schema=schema_gen.outputs['schema'],
+        # transform_graph=transform.outputs['transform_graph'],
+        train_args=trainer_pb2.TrainArgs(num_steps=configs.TRAIN_NUM_STEPS),
+        eval_args=trainer_pb2.EvalArgs(num_steps=configs.EVAL_NUM_STEPS),
+        custom_executor_spec=executor_spec.ExecutorClassSpec(ai_platform_trainer_executor.GenericExecutor),
+        custom_config={ai_platform_trainer_executor.TRAINING_ARGS_KEY: configs.GCP_AI_PLATFORM_TRAINING_ARGS}
+    )
+
+
+@lru_cache(maxsize=None)
+def model_resolver():
+    # Get the latest blessed model for model validation.
+    return resolver.Resolver(
+        strategy_class=latest_blessed_model_resolver.LatestBlessedModelResolver,
+        model=Channel(type=Model),
+        model_blessing=Channel(type=ModelBlessing)
+    ).with_id('latest_blessed_model_resolver')
+
+
+@lru_cache(maxsize=None)
+def evaluator(
+        example_gen=example_gen(),
+        trainer=trainer(),
+        model_resolver=model_resolver()
+):
+    # Uses TFMA to compute a evaluation statistics over features of a model and
+    # perform quality validation of a candidate model (compared to a baseline).
+
+    eval_config = tfma.EvalConfig(
+        model_specs=[tfma.ModelSpec(label_key=LABEL_KEY)],
+        slicing_specs=[tfma.SlicingSpec()],
+        metrics_specs=[
+            tfma.MetricsSpec(metrics=[
+                tfma.MetricConfig(
+                    class_name='BinaryAccuracy',
+                    threshold=tfma.MetricThreshold(
+                        value_threshold=tfma.GenericValueThreshold(
+                            lower_bound={'value': configs.EVAL_ACCURACY_THRESHOLD}),
+                        change_threshold=tfma.GenericChangeThreshold(direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                                                                     absolute={'value': -1e-10})
+                    )
+                )
+            ])
+        ])
+
+    return Evaluator(
+        examples=example_gen.outputs['examples'],
+        model=trainer.outputs['model'],
+        baseline_model=model_resolver.outputs['model'],
+        # Change threshold will be ignored if there is no baseline (first run).
+        eval_config=eval_config
+    )
+
+
+@lru_cache(maxsize=None)
+def pusher(
+        trainer=trainer()
+):
+    return Pusher(
+        model=trainer.outputs['model'],
+        # model_blessing=evaluator.outputs['blessing'],
+        # push_destination=pusher_pb2.PushDestination(filesystem=pusher_pb2.PushDestination.Filesystem(base_directory=configs.SERVING_MODEL_DIR)),
+        custom_executor_spec=executor_spec.ExecutorClassSpec(ai_platform_pusher_executor.Executor),
+        custom_config={ai_platform_pusher_executor.SERVING_ARGS_KEY: configs.GCP_AI_PLATFORM_SERVING_ARGS}
+    )
